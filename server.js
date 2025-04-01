e0a3fc3696a25ddbd85b5f28d5ee9b1b223cf52d
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2025-04-01 14:54:28 -0500

    initial commit | proxy server for insecure stream

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const app = express()
+
+app.use(
+  '/proxy-stream',
+  createProxyMiddleware({
+    target: 'http://kdhx-ice.streamguys1.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/proxy-stream': '/live',
+    },
+  })
+)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Proxy running on port ${port}`)
+})
